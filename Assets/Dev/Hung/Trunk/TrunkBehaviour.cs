8c715e03ffba3b8e06bc02297ba9c1fa802e4304
Nguyễn Quốc Hùng <107848000+HungNguyen20207677@users.noreply.github.com>
2023-05-01 01:44:33 +0700

    update trunk patrolling right direction

--- Assets/Dev/Hung/Trunk/TrunkBehaviour.cs
+++ Assets/Dev/Hung/Trunk/TrunkBehaviour.cs
@@ -25,13 +25,13 @@ public class TrunkBehaviour : MonoBehaviour
         if (IsFacingRight()) 
         {
             //Move right
-            myRigidbody.velocity = new Vector2(moveSpeed, 0f);
+            myRigidbody.velocity = new Vector2(-moveSpeed, 0f);
         }
 
         else 
         {
             //Move left
-            myRigidbody.velocity = new Vector2(-moveSpeed, 0f);
+            myRigidbody.velocity = new Vector2(moveSpeed, 0f);
         }
     }
 
@@ -43,6 +43,6 @@ public class TrunkBehaviour : MonoBehaviour
     private void OnTriggerExit2D(Collider2D collision)
     {
         //Turn
-        transform.localScale = new Vector2(-(Mathf.Sign(myRigidbody.velocity.x)),transform.localScale.y);
+        transform.localScale = new Vector2(-(Mathf.Sign(-myRigidbody.velocity.x)),transform.localScale.y);
     }
 }
