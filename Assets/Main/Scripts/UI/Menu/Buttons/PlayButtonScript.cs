8310ad4e2337f257700d0f7a738a66c1a3c583fb
Danny <danny@192.168.1.5>
2023-05-08 00:47:47 +0700

    finish basic 2 buttons for main menu

--- Assets/Main/Scripts/UI/Menu/Buttons/PlayButtonScript.cs
+++ Assets/Main/Scripts/UI/Menu/Buttons/PlayButtonScript.cs
@@ -8,6 +8,8 @@ public class PlayButtonScript : MonoBehaviour, IPointerUpHandler, IPointerDownHa
 {
     public Image img;
     public Sprite _default, _clicked;
+    public GameObject _text_normal, _text_compressed;
+    private bool _text_is_compressed = false;
 
     private void OnMouseDown()
     {
@@ -17,11 +19,26 @@ public class PlayButtonScript : MonoBehaviour, IPointerUpHandler, IPointerDownHa
     public void OnPointerDown(PointerEventData e)
     {
         img.sprite = _clicked;
+        _text_is_compressed = true;
     }
 
     public void OnPointerUp(PointerEventData e)
     {
         img.sprite = _default;
+        _text_is_compressed = false;
+    }
+
+    void Update()
+    {
+        ToggleTexts();
+    }
+
+    //custom methods
+
+    private void ToggleTexts()
+    {
+        _text_normal.SetActive(!_text_is_compressed);
+        _text_compressed.SetActive(_text_is_compressed);
     }
 }
 
