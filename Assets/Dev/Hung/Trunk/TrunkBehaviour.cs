6da6dc149bd194556d27b0049bc2a5d171a34fe5
Nguyễn Quốc Hùng <107848000+HungNguyen20207677@users.noreply.github.com>
2023-05-11 18:20:10 +0700

    complete terrain and background for a map

--- Assets/Dev/Hung/Trunk/TrunkBehaviour.cs
+++ Assets/Dev/Hung/Trunk/TrunkBehaviour.cs
@@ -37,7 +37,6 @@ public class TrunkBehaviour : MonoBehaviour
 
     private bool IsFacingRight()
     {
-        Debug.Log("hello1");
         return transform.localScale.x > Mathf.Epsilon;
     }
 
