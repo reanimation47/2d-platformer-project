e5cce20af384b8b89d62d84d6fc4e067f600b208
Nguyễn Quốc Hùng <107848000+HungNguyen20207677@users.noreply.github.com>
2023-05-17 02:18:33 +0700

    update obstacle & player dies logic

--- Assets/Dev/Hung/Trunk/TrunkBehaviour.cs
+++ Assets/Dev/Hung/Trunk/TrunkBehaviour.cs
@@ -6,14 +6,22 @@ public class TrunkBehaviour : MonoBehaviour
 {
     [SerializeField] float moveSpeed = 1f;
 
-    Rigidbody2D myRigidbody;
-
+    private Rigidbody2D myRigidbody;
+    private GameObject player;
+    private Transform player_transform;
     public Animator trunkAnimator;
     
     // Start is called before the first frame update
     void Start()
     {
-        myRigidbody = GetComponent<Rigidbody2D>();
+        myRigidbody = gameObject.GetComponent<Rigidbody2D>();
+        player = ICommon.GetPlayerObject();
+        
+
+        if (player)
+        {
+            player_transform = player.GetComponent<Transform>();
+        }
 
     }
 
@@ -33,6 +41,7 @@ public class TrunkBehaviour : MonoBehaviour
             //Move left
             myRigidbody.velocity = new Vector2(moveSpeed, 0f);
         }
+
     }
 
     private bool IsFacingRight()
@@ -44,5 +53,31 @@ public class TrunkBehaviour : MonoBehaviour
     {
         //Turn
         transform.localScale = new Vector2(-(Mathf.Sign(-myRigidbody.velocity.x)),transform.localScale.y);
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.LogError("Hit");
+        if (collision.gameObject == player)
+        {
+            int collide_state = ICommon.GetCollisionDirection(collision);
+
+            if (collide_state == 0)
+            {
+                ICommon.KillPlayer();
+            }else if (collide_state ==1)
+            {
+                //anim
+                //Destroy(gameObject);
+                StartCoroutine(Trunkdies());
+            }
+        }
+    }
+
+    IEnumerator Trunkdies()
+    {
+        trunkAnimator.SetTrigger("Die");
+        yield return new WaitForSeconds(0.2f);
+        Destroy(gameObject);
     }
 }
