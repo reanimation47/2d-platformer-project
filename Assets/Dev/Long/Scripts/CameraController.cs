46d3e5c93c928732aca030ba42427a5acf46fecb
Danny <danny@Dannys-MacBook-Air.local>
2023-05-26 15:44:30 +0700

    fix rockhead movement

--- Assets/Dev/Long/Scripts/CameraController.cs
+++ Assets/Dev/Long/Scripts/CameraController.cs
@@ -4,11 +4,15 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    [SerializeField] private Transform player;
-
+    private Transform player;
+    private void Start()
+    {
+        player = ICommon.GetPlayerObject().transform;
+    }
     // Update is called once per frame
     private void Update()
     {
+        if (!player) { return; }
         transform.position = new Vector3(player.position.x,player.position.y,transform.position.z);
     }
 }
