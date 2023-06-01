4249a30eab3e29ba21b06bd4ea482ae31524257c
Danny <danny@192.168.6.102>
2023-06-01 23:32:54 +0700

    backup

---
+++ Assets/Main/Scripts/UI/Menu/Buttons/BackButtonScript.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+using static UnityEditorInternal.ReorderableList;
+
+public class BackButtonScript : MonoBehaviour, IPointerUpHandler, IPointerDownHandler
+{
+    private Vector3 scaler = new Vector3(1, 1, 1);
+    private Vector3 normal = new Vector3(1, 1, 1);
+    private Vector3 shrinker = new Vector3(0.8f, 0.8f, 1);
+    private float target = 1;
+    RectTransform rectTransform;
+
+    void Start()
+    {
+        rectTransform = GetComponent<RectTransform>();
+    }
+
+    private void Update()
+    {
+        rectTransform.localScale = scaler;
+    }
+
+    private void FixedUpdate()
+    {
+        scaler.x = Mathf.Lerp(scaler.x, target, 0.3f);
+        scaler.y = Mathf.Lerp(scaler.y, target, 0.3f);
+    }
+
+    public void OnPointerDown(PointerEventData e)
+    {
+        target = 0.8f;
+    }
+
+    public void OnPointerUp(PointerEventData e)
+    {
+        target = 1f;
+    }
+}
+
