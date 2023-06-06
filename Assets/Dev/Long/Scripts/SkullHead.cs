4494ee3bbdb79452743b523df856e69fcbff9d6c
LongNguyenVu24 <86005133+LongNguyenVu24@users.noreply.github.com>
2023-06-06 13:55:39 +0700

    skull-2
    just skull has two more anims, the moving method is coming soon

--- Assets/Dev/Long/Scripts/SkullHead.cs
+++ Assets/Dev/Long/Scripts/SkullHead.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class SkullHead : MonoBehaviour
 {
-    // Start is called before the first frame update
+    //For Idle Stage
+    [Header("Idle")]
+    public float idleMoveSpeed;
+    public Vector2 idleMoveDirection;
+
+    //For Attack stage
+    [Header("Attack")]
+    public float attackMoveSpeed;
+    public Vector2 attackMoveDirection;
+
+
+    private float attackPlayerSpeed;
+    public Transform player;
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject == IPlayer.GetPlayerObject())
+        {
+            IPlayer.KillPlayer();
+        }
     }
 }
