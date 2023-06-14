1499251eec33c67bafb8e4f875e2a270d2669b57
Danny <danny@192.168.1.5>
2023-06-14 23:07:39 +0700

    refactor enemytype + add spikehead card for stageselect popup

---
+++ Assets/Main/Scripts/UI/Menu/StageSelect/EnemyTypeEnum.cs
@@ -0,0 +1,13 @@
+namespace Enum.StageSelect.EnemyTypes
+{
+    public enum EnemyType
+    {
+        BlueTurtle = 0,
+        FatBird = 1,
+        ChargeRino = 2,
+        TeleGhost = 3,
+        BulletTrunk = 4,
+        SpikeHead = 5
+
+    }
+}
