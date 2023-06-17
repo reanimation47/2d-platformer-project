f82f7e4c58a83c351c4d92392194042208b8554f
Danny <danny@192.168.1.5>
2023-06-18 02:36:12 +0700

    WIP stage complete screen

--- Assets/Main/Scripts/UI/InGame/Screens/StageComplete/StageCompleteButton.cs
+++ Assets/Main/Scripts/UI/InGame/Screens/StageComplete/StageCompleteButton.cs
@@ -1,19 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.EventSystems;
-using UnityEngine.SceneManagement;
-using UnityEngine.UIElements;
 
-public class GameOverButton : InGameButtonBase
+public class StageCompleteButton : InGameButtonBase
 {
     public override void ConfirmAction()
     {
-        InGameCanvasInterface.RestartGame();
+        
     }
     public override void QuitAction()
     {
         InGameCanvasInterface.BackToStageSelect();
     }
-
 }
