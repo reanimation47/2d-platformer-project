cb8ed2c7cdbdcfb699df18b4600ca0f472261949
Danny <danny@192.168.1.5>
2023-07-02 21:04:30 +0700

    implement different characters in game

--- Assets/Main/Scripts/Common/CharacterSpawner/CharacterSpawner.cs
+++ Assets/Main/Scripts/Common/CharacterSpawner/CharacterSpawner.cs
@@ -7,9 +7,19 @@ public class CharacterSpawner : MonoBehaviour
 {
     public List<GameObject> Characters;
     private int CharacterIndex;
-    private void Start()
+    private void Awake()
     {
         CharacterIndex = PlayerPrefs.GetInt(CharacterSelectInfo.CharacterSelected_PlayerPrefs_Key);
-        Characters[CharacterIndex].SetActive(true);
+        Instantiate(Characters[CharacterIndex], transform.position, Quaternion.identity);
+        //Characters[CharacterIndex].SetActive(true);
+        //Activating just the right character caused a "data race" issue which isn't worth to fix rn
+
+        //for (int i = 0; i < Characters.Count; i ++) //Enable all at default and deactivate all the wrong characters instead.
+        //{
+        //    if (i != CharacterIndex)
+        //    {
+        //        Characters[i].SetActive(false);
+        //    }
+        //}
     }
 }
